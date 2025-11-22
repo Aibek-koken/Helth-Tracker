fbfa32de2101bd89ad462dd13b7597a059d79b5f
Aibek <zharylkassynaibek@gmail.com>
2025-11-22 18:56:35 +0500

    проверка .yml

--- src/test/java/com/helthtracer/HealthTracerApplicationIntegrationTest.java
+++ src/test/java/com/helthtracer/HealthTracerApplicationIntegrationTest.java
@@ -5,11 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
+@TestPropertySource(properties = {
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration"
+})
 public class HealthTracerApplicationIntegrationTest {
 
     @Autowired
