1c70ca9663d962aeead91d5d2f4a694e216d0b7b
Aibergen <240118007@sdu.edu.kz>
2025-12-04 14:36:50 +0500

    Aibergen v dele

--- src/main/java/com/helthtracer/HealthTracerApplication.java
+++ src/main/java/com/helthtracer/HealthTracerApplication.java
@@ -2,7 +2,7 @@ package com.helthtracer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//оооо
 @SpringBootApplication
 public class HealthTracerApplication {
     public static void main(String[] args) {
